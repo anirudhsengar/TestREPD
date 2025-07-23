a1fc040011f684f94e79b427112107e2178e7319
Anirudh Sengar <anirudhsengar3@gmail.com>
2025-07-23 12:00:57 +0400

    Add buggy files

---
+++ test_files/buggy_file_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+class GC {
+public:
+    void collect() {
+        // Bug: Use-after-free
+        std::vector<int>* vec = new std::vector<int>();
+        vec->push_back(1);
+        delete vec;
+        std::cout << (*vec)[0] << std::endl;
+    }
+};
+
+int main() {
+    GC gc;
+    gc.collect();
+    return 0;
+}
